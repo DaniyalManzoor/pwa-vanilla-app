5264332fa420f6ba51da31722c541c8e102df1bb
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-08-15 15:42:36 +0500

    fetch event lister

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
 //install services worker
 self.addEventListener("install", (evt) => {
-  console.log("services worker has been installed");
+  console.log("services worker has been installed", evt);
 });
 
 //activate event
 self.addEventListener("activate", (evt) => {
-  console.log("service worker has been activate");
+  console.log("service worker has been activate", evt);
+});
+
+//fetch event
+self.addEventListener("fetch", (evt) => {
+  console.log("fetch Event", evt);
 });
