d790eba66625f4eaff571dde7e1f714c9ed6ff33
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-08-15 16:32:38 +0500

    pre-cacheing assets

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/main.js",
+  "style.css",
+  //"logo.png"
+  "https://fonts.googlepis.com/icon?family=Material+Icon",
+];
+
 //install services worker
 self.addEventListener("install", (evt) => {
-  console.log("services worker has been installed", evt);
+  //console.log("services worker has been installed", evt);
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
 });
 
 //activate event
