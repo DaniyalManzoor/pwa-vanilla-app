5065f17b58a8daf6c0007da0ed885a6735cd3c6c
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-08-16 10:24:49 +0500

    dynamic cache

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = "site-static-v1";
+const dynamicCache = "dynamic-site-v1";
 const assets = [
   "/",
   "/index.html",
@@ -37,7 +38,15 @@ self.addEventListener("fetch", (evt) => {
   // console.log("fetch Event", evt);
   evt.respondWith(
     caches.match(evt.request).then((cacheRes) => {
-      return cacheRes || fetch(evt.request);
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCache).then((cache) => {
+            cache.put(evt.request.url, fetchRes.clone);
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
